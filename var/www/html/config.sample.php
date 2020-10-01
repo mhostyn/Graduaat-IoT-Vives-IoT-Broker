5918c952cdd37167fa11275854e2179d8680f0d5
Maarten Hostyn <mhostyn@users.noreply.github.com>
2020-10-02 00:36:32 +0200

    Add sample config and gitignore for local config

---
+++ var/www/html/config.sample.php
@@ -0,0 +1,16 @@
+<!--
+    Dit is een voorbeeldconfiguratiebestand.
+    
+    Gelieve deze te hernoemen naar config.php
+
+    en vervolgens naar smaak aan te passen waar nodig
+-->
+
+
+<?php
+    // de database login gegevens
+    $dbhost = 'localhost';
+    $dbuser = 'webuser';
+    $dbpass = 'secretpassword';
+    $dbname = 'globe_bank';
+?>
