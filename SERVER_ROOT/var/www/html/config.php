9fad740b1891681940a9afcf53d9c2ebb82f2136
Maarten Hostyn <mhostyn@users.noreply.github.com>
2020-10-03 17:03:49 +0200

    Revert "update .gitignore"
    This reverts commit 3ab329c5edc55779951db9a123708bfb94ee3e34.

--- SERVER_ROOT/var/www/html/config.php
+++ SERVER_ROOT/var/www/html/config.php
@@ -5,13 +5,17 @@
 
     en vervolgens naar smaak aan te passen waar nodig
 
+
 aa6c05c0-8709-4c4c-abc0-dbc3409cc745
 d34db33f-1111-1111-1111-1111d34db33f
 
 6447c1a4-c3c1-4584-bf76-6af46ca86310
 d34db33f-2222-2222-2222-2222d34db33f
 
+
+
 -->
+
 
 <?php
     // de database login gegevens
