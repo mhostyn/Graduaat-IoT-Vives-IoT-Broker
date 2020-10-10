9b4e45d4a6dbdaa6dfea030324e74d895e4b09f1
Maarten Hostyn <mhostyn@users.noreply.github.com>
2020-10-10 19:51:02 +0200

    herwerkte database script
    met dank aan Mathias die wat aanpassingen voorzien heeft bij het ontdekken van fouten tijdens zijn tests hebben we een compleet script database+user

--- scripts/sql/001_CREATE_iot-broker-db.sql
+++ scripts/sql/001_CREATE_iot-broker-db.sql
@@ -1,120 +1,59 @@
--- phpMyAdmin SQL Dump
--- version 5.0.2
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Sep 29, 2020 at 11:08 PM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.4.10
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
--- Database: `iot-broker-db`
+-- Database: `developersDashboard`
 --
-DROP DATABASE IF EXISTS `iot-broker-db_dev`;
-CREATE DATABASE IF NOT EXISTS `iot-broker-db_dev` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `iot-broker-db_dev`;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `data`
---
-
-CREATE TABLE `data` (
-  `data_id` int(11) NOT NULL,
-  `value` double NOT NULL,
-  `timestamp` timestamp NOT NULL DEFAULT current_timestamp(),
-  `sensor_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `sensor`
---
-
-CREATE TABLE `sensor` (
-  `sensor_id` int(11) NOT NULL,
-  `name` varchar(32) NOT NULL,
-  `unit` enum('°C','°F','°K','BAR','psi','%','mV') NOT NULL,
-  `type` enum('temperature','pressure','humidity','battery_pct','battery_mV') NOT NULL,
-  `created` timestamp NOT NULL DEFAULT current_timestamp(),
-  `user_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
+DROP DATABASE IF EXISTS `developersDashboard`;
+CREATE DATABASE IF NOT EXISTS `developersDashboard` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE developersDashboard;
 
 --
 -- Table structure for table `user`
 --
-
-CREATE TABLE `user` (
-  `user_id` int(11) NOT NULL,
-  `firstname` varchar(50) NOT NULL,
-  `lastname` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `vkey` varchar(32) DEFAULT 'uuid()',
-  `api_key` varchar(32) NOT NULL DEFAULT 'uuid()',
-  `status` set('vertification_sent','reset_requested','vertified','vertification_timeout') NOT NULL,
-  `created` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Indexes for dumped tables
---
+CREATE TABLE user (
+  user_id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  firstname varchar(50) NOT NULL,
+  lastname varchar(50) NOT NULL,
+  password varchar(50) NOT NULL,
+  email varchar(50) NOT NULL,
+  vkey varchar(32) DEFAULT 'uuid()',
+  api_key varchar(32) NOT NULL DEFAULT 'uuid()',
+  status set('vertification_sent','reset_requested','vertified','vertification_timeout') NOT NULL,
+  created timestamp NOT NULL DEFAULT current_timestamp()
+);
 
 --
--- Indexes for table `data`
+-- Table structure for table `sensor`
 --
-ALTER TABLE `data`
-  ADD PRIMARY KEY (`data_id`);
+CREATE TABLE sensor (
+  sensor_id int(11) NOT NULL UNIQUE auto_increment,
+  name varchar(255) NOT NULL UNIQUE,
+  unit varchar(255) NOT NULL,
+  type varchar(255) NOT NULL,
+  created timestamp NOT NULL DEFAULT current_timestamp(),
+  user_id int(11) NOT NULL,
+  PRIMARY KEY (sensor_id),
+  FOREIGN KEY (user_id) REFERENCES user(user_id)
+);
 
 --
--- Indexes for table `sensor`
+-- Table structure for table `data`
 --
-ALTER TABLE `sensor`
-  ADD PRIMARY KEY (`sensor_id`);
+CREATE TABLE data (
+data_id INT NOT NULL AUTO_INCREMENT,
+value INT NOT NULL,
+timestamp DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+sensor_id INT NOT NULL,
+PRIMARY KEY (data_id),
+FOREIGN KEY (sensor_id) REFERENCES sensor(sensor_id) 
+);
 
 --
--- Indexes for table `user`
+-- Create new user / grant permissions
 --
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`user_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `data`
---
-ALTER TABLE `data`
-  MODIFY `data_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `sensor`
---
-ALTER TABLE `sensor`
-  MODIFY `sensor_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+USE developersDashboard;
+DROP USER IF EXISTS 'develeporsUser'@'localhost';
+CREATE USER 'develeporsUser'@'localhost' IDENTIFIED BY 'hFRfjCptDik9RQHH';
+GRANT ALL ON developersDashboard.* TO 'develeporsUser'@'localhost';
