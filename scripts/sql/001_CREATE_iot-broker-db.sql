5ad8eb356236c34b1eae9c82ddaec7f1a5b40fea
Maarten Hostyn <mhostyn@users.noreply.github.com>
2020-10-11 03:34:20 +0200

    Update 001_CREATE_iot-broker-db.sql

--- scripts/sql/001_CREATE_iot-broker-db.sql
+++ scripts/sql/001_CREATE_iot-broker-db.sql
@@ -1,7 +1,6 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
 --
 -- Database: `iotBrokerDB`
 --
@@ -9,39 +8,40 @@ DROP DATABASE IF EXISTS `iotBrokerDB`;
 CREATE DATABASE `iotBrokerDB` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `iotBrokerDB`;
 CREATE TABLE `user` (
-    `user_id` INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `firstname` VARCHAR(50) NOT NULL,
-    `lastname` VARCHAR(50) NOT NULL,
-    `password` VARCHAR(50) NOT NULL,
-    `email` VARCHAR(50) NOT NULL,
-    `vkey` VARCHAR(32) NOT NULL,
-    `api_key` VARCHAR(32) NOT NULL,
-    `status` SET('vertification_sent', 'reset_requested', 'vertified', 'vertification_timeout') NOT NULL,
+  `user_id` INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `firstname` VARCHAR(50) NOT NULL,
+  `lastname` VARCHAR(50) NOT NULL,
+  `password` VARCHAR(50) NOT NULL,
+  `email` VARCHAR(50) NOT NULL,
+  `vkey` VARCHAR(32) NOT NULL,
+  `api_key` VARCHAR(32) NOT NULL,
+  `status`
+  SET(
+      'vertification_sent',
+      'reset_requested',
+      'vertified',
+      'vertification_timeout'
+    ) NOT NULL,
     `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ()
 );
-
 CREATE TABLE `sensor` (
-    `sensor_id` INT NOT NULL UNIQUE AUTO_INCREMENT,
-    `name` VARCHAR(255) NOT NULL UNIQUE,
-    `unit` VARCHAR(255) NOT NULL,
-    `type` VARCHAR(255) NOT NULL,
-    `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP (),
-    `user_id` INT NOT NULL,
-    PRIMARY KEY (`sensor_id`),
-    FOREIGN KEY (`user_id`)
-        REFERENCES `user` (`user_id`)
+  `sensor_id` INT NOT NULL UNIQUE AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL UNIQUE,
+  `unit` VARCHAR(255) NOT NULL,
+  `type` VARCHAR(255) NOT NULL,
+  `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP (),
+  `user_id` INT NOT NULL,
+  PRIMARY KEY (`sensor_id`),
+  FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
 );
-
 CREATE TABLE `data` (
-    `data_id` INT NOT NULL AUTO_INCREMENT,
-    `value` INT NOT NULL,
-    `timestamp` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `sensor_id` INT NOT NULL,
-    PRIMARY KEY (`data_id`),
-    FOREIGN KEY (`sensor_id`)
-        REFERENCES `sensor` (`sensor_id`)
+  `data_id` INT NOT NULL AUTO_INCREMENT,
+  `value` INT NOT NULL,
+  `timestamp` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `sensor_id` INT NOT NULL,
+  PRIMARY KEY (`data_id`),
+  FOREIGN KEY (`sensor_id`) REFERENCES `sensor` (`sensor_id`)
 );
-
 --
 -- Create new user / grant permissions
 --
@@ -55,3 +55,15 @@ GRANT SELECT,
   INSERT,
   UPDATE,
   DELETE ON `iotBrokerDB`.* TO 'webuser' @'localhost';
+/* voor Node-Red
+ Error: ER_NOT_SUPPORTED_AUTH_MODE: 
+ Client does not support authentication protocol requested by server; 
+ consider upgrading MySQL client
+ */
+DROP USER IF EXISTS 'node-red' @'localhost';
+CREATE USER 'node-red' @'localhost' IDENTIFIED WITH mysql_native_password BY 'hFRfjCptDik9RQHH';
+GRANT SELECT,
+  INSERT,
+  UPDATE,
+  DELETE ON `iotBrokerDB`.* TO 'node-red' @'localhost';
+FLUSH PRIVILEGES;
