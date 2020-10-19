1ef97f50101e3df2ce124851006f2582194720b1
Mathias-School <66418685+Mathias-School@users.noreply.github.com>
2020-10-19 19:47:58 +0200

    Version 18/10/2020
    Session included

--- SERVER_ROOT/team1/php/card_display.php
+++ SERVER_ROOT/team1/php/card_display.php
@@ -2,11 +2,11 @@
  global $numberOfCards;
 
  //--|-1 for offset array
- for ($x = 0; $x <= ($numberOfCards - 1); $x++) {
+ for ($cardIndex = 0; $cardIndex <= ($numberOfCards - 1); $cardIndex++) {
 
     //--|CREATE A VARIABLE THAT HOLDS THE LAST VALUE OF THE TABLE
     $last_value = "";
-    foreach($this->cards->tableValues[$x] as $value){
+    foreach($this->cards->tableValues[$cardIndex] as $value){
         $last_value = (end($value));
     }
 
@@ -19,14 +19,15 @@
         </div>
         <h4 class="card__heading">
             <span class="card__heading-span card__heading-span-1">
-            '.$this->cards->values[$x]["name"].'
+            '.$this->cards->values[$cardIndex]["name"].'
             </span>
         </h4>
         <div class="card__details">
             <ul>
-                <li class="card_details_ids">Sensor id: '.$this->cards->values[$x]["sensor_id"].'</li>
+                <li class="card_details_ids">Sensor id: '.$this->cards->values[$cardIndex]["sensor_id"].'</li>
                 <li>Last value: '.$last_value.'</li>
-                <li>Type: '.$this->cards->values[$x]["type"].'</li>
+                <li>Type: '.$this->cards->values[$cardIndex]["type"].'</li>
+                <li>Unit: '.$this->cards->values[$cardIndex]["unit"].'</li>
             </ul>
         </div>
     </div>
@@ -36,29 +37,36 @@
 
             <form action="php/delete.php" method="POST">
             <input type="hidden" name="action" value="delete" />
-            <input type="hidden" name="sensor_id" value='.$this->cards->values[$x]["sensor_id"].'/>
+            <input type="hidden" name="sensor_id" value='.$this->cards->values[$cardIndex]["sensor_id"].'/>
             <input class="popup__close--delete" type="submit" value="Delete sensor &#8595"/>
             </form>
 
             <div class="card__table__th">
             </div>
             <table class="card__table">
-            
-         ';
+            ';
 
-         foreach($this->cards->tableValues[$x] as $value){
+         foreach($this->cards->tableValues[$cardIndex] as $value){
             echo '
-            <tr class="card__table__tr">
-            <td class="card__table__td__value">'.$value["value"].'</td>
-            </tr>
+                <tr class="card__table__tr">
+                <td class="card__table__td__value"> Value: '.$value["value"].' </td>
+                </tr>
+            ';
+        }
+            foreach($this->cards->tableIds[$cardIndex] as $value){
+                echo'
+                <tr class="card__table__tr">
+                <td class="card__table__td__value"> Id: '.$value["data_id"].'</td>
+                </tr>
                 ';
          }
 
-         echo '
+         echo '  
          </table>
             </div>
             <a class="container" href="#popup">
-            <input class="chartDisplayers" type="button" value="view chart!"/>
+            <!-- <input class="chartDisplayers" type="button" data-target="sensor_id_'.$cardIndex.'" value="view chart!"/> -->
+            <input class="chartDisplayers" type="button" data-target="sensor_id_'.$cardIndex.'" value="view chart!"/>
             </a>
         </div>
     </div>
@@ -67,8 +75,3 @@
 ';
   }
 ?>
-
-
-
-
-<!-- <td class="card__table__td__number">'.$this->cards->dataid[$i]["data_id"].'</td> -->
