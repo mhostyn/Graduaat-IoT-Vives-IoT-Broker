7455e9b943f6ab2f502695d33927f69642ae977b
Mathias-School <66418685+Mathias-School@users.noreply.github.com>
2020-10-24 17:44:32 +0200

    Create header.php

---
+++ SERVER_ROOT/php/header.php
@@ -0,0 +1,20 @@
+<?php 
+// require_once "controllerUserData.php"; 
+?>
+
+
+<!-- Navbar start -->
+<nav class="navbar">
+        <div class="max-width">
+          
+            <div class="logo">
+<a href="./index.html">IoT <span>Broker.</span></a></div>
+<ul class="menu">
+  <li><a href="#" class="menu-btn">Dag <?php echo $fetch_info['firstname']?>!</a></li>
+  <li><a href="./logout.php" class="menu-btn-logout">Logout</a></li>
+</ul>
+<div class="menu-btn">
+    <i class="fas fa-bars"></i>
+</div>
+</div>
+</nav>
