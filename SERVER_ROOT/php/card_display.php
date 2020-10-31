d8409bd7a1d09879bc580ff384a41b83e301ff3f
Mathias-School <66418685+Mathias-School@users.noreply.github.com>
2020-10-31 19:12:40 +0100

    Update card_display.php
    Last upload

--- SERVER_ROOT/php/card_display.php
+++ SERVER_ROOT/php/card_display.php
@@ -1,76 +1,75 @@
 <?php
- global $numberOfCards;
 
- //--|-1 for offset array
- for ($cardIndex = 0; $cardIndex <= ($numberOfCards - 1); $cardIndex++) {
 
-    //--|CREATE A VARIABLE THAT HOLDS THE LAST VALUE OF THE TABLE
-    $last_value = "";
-    foreach($this->cards->tableValues[$cardIndex] as $value){
-        $last_value = (end($value));
-    }
+global $numberOfCards;
 
-    echo '
-    <div class="card" id="card">
-    <div class="card__side card__side--front">
-        <div class="card__picture card__picture--1">
-            &nbsp;
-            <!-- <- Empy  space -->
-        </div>
-        <h4 class="card__heading">
-            <span class="card__heading-span card__heading-span-1">
-            '.$this->cards->values[$cardIndex]["name"].'
-            </span>
-        </h4>
-        <div class="card__details">
-            <ul>
-                <li class="card_details_ids">Sensor id: '.$this->cards->values[$cardIndex]["sensor_id"].'</li>
-                <li>Last value: '.$last_value.'</li>
-                <li>Type: '.$this->cards->values[$cardIndex]["type"].'</li>
-                <li>Unit: '.$this->cards->values[$cardIndex]["unit"].'</li>
-            </ul>
-        </div>
-    </div>
-    <div class="card__side card__side--back card__side--back-1">
-        <div class="card__cta">
-            <div>
+//--|-1 for offset array
+for ($cardIndex = 0; $cardIndex <= ($numberOfCards - 1); $cardIndex++) {
+   echo '
+   <div class="card" id="card">
+   <div class="card__side card__side--front">
+       <div class="card__picture card__picture--1">
+           &nbsp;
+           <!-- <- Empy  space -->
+       </div>
+       <h4 class="card__heading">
+           <span class="card__heading-span card__heading-span-1">
+           '.html($this->cards->values[$cardIndex]["name"]).'
+           </span>
+       </h4>
+       <div class="card__details">
+           <ul>
+               <li class="card_details_ids">Sensor id: '.html($this->cards->values[$cardIndex]["sensor_id"]).'</li>
+               ';
+               if(!isset($this->cards->lastValue[$cardIndex][0]["value"])){
+                $this->cards->lastValue[$cardIndex][0]["value"] = "/";
+               }
 
-            <form action="php/delete.php" method="POST">
-            <input type="hidden" name="action" value="delete" />
-            <input type="hidden" name="sensor_id" value='.$this->cards->values[$cardIndex]["sensor_id"].'/>
-            <input class="popup__close--delete" type="submit" value="Delete sensor &#8595"/>
-            </form>
+               echo '
+               <li>Last value: '.html($this->cards->lastValue[$cardIndex][0]["value"]).'</li>
+               <li>Type: '.html($this->cards->values[$cardIndex]["type"]).'</li>
+               <li>Unit: '.html($this->cards->values[$cardIndex]["unit"]).'</li>
+           </ul>
+       </div>
+   </div>
+   <div class="card__side card__side--back card__side--back-1">
+       <div class="card__cta">
+           <div>
 
-            <div class="card__table__th">
-            </div>
-            <table class="card__table">
-            ';
+           <form action="php/delete.php" method="POST">
+           <input type="hidden" name="action" value="delete" />
+           <input type="hidden" name="sensor_id" value='.html($this->cards->values[$cardIndex]["sensor_id"]).'/>
+           <input class="popup__close--delete__sensor" type="submit" value="Delete sensor &#8595"/>
+           </form>
 
-         foreach($this->cards->tableValues[$cardIndex] as $key => $value){
-            echo '
-                <tr class="card__table__tr">
-                <form action="php/delete.php" method="POST">
-                <input type="hidden" name="action" value="deleteButton" />
-                <input type="hidden" name="data_id" value='.$this->cards->tableIds[$cardIndex][$key]["data_id"].'/>
+           <div class="card__table__th">
+           </div>
+           <table class="card__table">
+           ';
 
-                <td class="card__table__td__value"> Value: '.$value["value"].' </td>
-                <td class="card__table__td__value">Id: '.$this->cards->tableIds[$cardIndex][$key]["data_id"].' <input type="submit" value="Del"/></td>
-                </form>
-                </tr>
-            ';
-        }
-         echo '  
-         </table>
-            </div>
-            <a class="container" href="#popup">
-            <!-- <input class="chartDisplayers" type="button" data-target="sensor_id_'.$cardIndex.'" value="view chart!"/> -->
-            <input class="chartDisplayers" type="button" data-target="sensor_id_'.$cardIndex.'" value="view chart!"/>
-            </a>
-        </div>
-    </div>
+        foreach($this->cards->tableValues[$cardIndex] as $key => $value){
+           echo '
+               <tr class="card__table__tr">
+               <form action="php/delete.php" method="POST">
+               <input type="hidden" name="action" value="deleteButton" />
+               <input type="hidden" name="data_id" value='.html($this->cards->tableIds[$cardIndex][$key]["data_id"]).'/>
+               <td class="card__table__td__value"> Value: '.html($value["value"]).' </td>
+               <td class="card__table__td__value">Id: '.html($this->cards->tableIds[$cardIndex][$key]["data_id"]).' <input class="popup__close--delete__data" type="submit" value="Del"/></td>
+               </form>
+               </tr>
+           ';
+       }
+        echo '  
+        </table>
+           </div>
+           <a class="container" href="#popup">
+           <!-- <input class="chartDisplayers" type="button" data-target="sensor_id_'.html($cardIndex).'" value="view chart!"/> -->
+           <input class="chartDisplayers" type="button" data-target="sensor_id_'.html($cardIndex).'" value="view chart!"/>
+           </a>
+       </div>
+   </div>
 </div>
 </div>
 ';
-  }
-
+ }
 ?>
