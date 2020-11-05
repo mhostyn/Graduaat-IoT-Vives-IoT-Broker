50e969269f073683aa048cead66472eec03405b6
Maarten Hostyn <mhostyn@users.noreply.github.com>
2020-11-05 23:56:45 +0100

    Update db_delete_sensor.php
    $sensor_id = rtrim(db_escape($connection, $_POST["sensor_id"]), "/");
    has to be
    $sensor_id = db_escape($connection, rtrim($_POST["sensor_id"]), "/");

--- SERVER_ROOT/php/db_delete_sensor.php
+++ SERVER_ROOT/php/db_delete_sensor.php
@@ -5,10 +5,10 @@ require_once('functions.php');
 $connection = db_connect();
 
 //---|BUILD QUERY
- if(is_post_request()) {
-        if($_POST["action"]=='delete'){
+if (is_post_request()) {
+        if ($_POST["action"] == 'delete') {
                 //--|REMOVE "/" FROM $_POST
-                $sensor_id = rtrim(db_escape($connection, $_POST["sensor_id"]), "/");
+                $sensor_id = db_escape($connection, rtrim($_POST["sensor_id"]), "/");
                 //--|BUILD QUERY
                 $delete_data = "DELETE FROM data WHERE sensor_id ={$sensor_id}; ";
                 $delete_sensor = "DELETE FROM sensor WHERE sensor_id={$sensor_id}; ";
@@ -17,13 +17,13 @@ $connection = db_connect();
                 mysqli_query($connection, $delete_sensor);
         }
 
-        if($_POST["action"]=='deleteButton'){
-                 //--|REMOVE "/" FROM $_POST
-                 $data_id = rtrim(db_escape($connection, $_POST["data_id"]), "/");
-                 //--|BUILD QUERY
-                 $delete_data_id = "DELETE FROM data WHERE data_id ={$data_id}; ";
-                 //--|PERFORM QUERY
-                 mysqli_query($connection, $delete_data_id);
+        if ($_POST["action"] == 'deleteButton') {
+                //--|REMOVE "/" FROM $_POST
+                $data_id = db_escape($connection, rtrim($_POST["data_id"]), "/");
+                //--|BUILD QUERY
+                $delete_data_id = "DELETE FROM data WHERE data_id ={$data_id}; ";
+                //--|PERFORM QUERY
+                mysqli_query($connection, $delete_data_id);
         }
 }
 //--|CLOSE CONNECTION
@@ -34,4 +34,3 @@ redirect_to('../dashboard.php');
 
 //--|TELL CHROME THIS IS THE END OF THE FILE (GOOD PRACTICE)
 exit();
-?>
