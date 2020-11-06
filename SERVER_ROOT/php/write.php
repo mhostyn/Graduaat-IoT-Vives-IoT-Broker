a4069b33196df19b2b1802e2639d22e62be5addc
iTzBrilliiiant <63455856+iTzBrilliiiant@users.noreply.github.com>
2020-11-06 16:46:47 +0100

    Update write.php

--- SERVER_ROOT/php/write.php
+++ SERVER_ROOT/php/write.php
@@ -1,38 +1,75 @@
 <?php
 require_once('db_config.php');
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-echo "Connection successfully";
 
+$api_key_uservalue1 = "d34db33f11111";
+$api_key_uservalue2 = "d34db33f22222";
 
-//querry uitvoeren
-    if($_SERVER['REQUEST_METHOD']=='POST')
-    {
-        $Sensorid=mysqli_real_escape_string($connection,$_POST['']);
-        $Lichtsterkte=mysqli_real_escape_string($connection,$_POST['Lux']);
-        $insert = "INSERT INTO sensor (Sensorid, Lichtsterkte,)
-        WHERE user_id ='d34db33f11111';
-        VALUES (now(),'{$Sensorid}','{$Lichtstrekte}');";
-         mysqli_query($connection,$insert);
+$api_key= $sensor = $location = $value1 = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $api_key = test_input($_POST["api_key"]);
+    if($api_key == $api_key_uservalue1) {
+        $sensorid = test_input($_POST[""]);
+        $lichtsterkte = test_input($_POST["lux"]);
+       
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        } 
         
-    }  
+        $sql = "INSERT INTO sensor (sensorid,lichtsterkte)
+        VALUES ('" . $sensorid . "',  '" . $lichtsterkte . "')";
+        
+        if ($conn->query($sql) === TRUE) {
+            echo "New record created successfully";
+        } 
+        else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+    
+        $conn->close();
+    }
    
-    if else($_SERVER['REQUEST_METHOD']=='POST')
-    {
-        $Sensorid=mysqli_real_escape_string($connection,$_POST['']);
-        $Lichtsterkte=mysqli_real_escape_string($connection,$_POST['Lux']);
-        $insert = "INSERT INTO sensor (Sensorid, Lichtsterkte,)
-        WHERE user_id ='d34db33f22222';
-        VALUES (now(),'{$Sensorid}','{$Lichtstrekte}');";
-         mysqli_query($connection,$insert);
+    if else ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $api_key = test_input($_POST["api_key"]);
+        if($api_key == $api_key_uservalue2) {
+            $sensorid = test_input($_POST[""]);
+            $lichtsterkte = test_input($_POST["lux"]);
+           
+            // Create connection
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            // Check connection
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            } 
+            
+            $sql = "INSERT INTO sensor (sensorid,lichtsterkte)
+            VALUES ('" . $sensorid . "',  '" . $lichtsterkte . "')";
+            
+            if ($conn->query($sql) === TRUE) {
+                echo "New record created successfully";
+            } 
+            else {
+                echo "Error: " . $sql . "<br>" . $conn->error;
+            }
         
-    }  
+            $conn->close();
+        }
+    else {
+        echo "Wrong API Key provided.";
+    }
 
-    $conn->close();
-?>
+}
+else {
+    echo "No data posted with HTTP POST.";
+}
 
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
