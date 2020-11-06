5b091f4155a088c0d01d5b57628c145f1e48146b
iTzBrilliiiant <63455856+iTzBrilliiiant@users.noreply.github.com>
2020-11-06 20:21:32 +0100

    Update write.php

--- SERVER_ROOT/php/write.php
+++ SERVER_ROOT/php/write.php
@@ -1,6 +1,22 @@
 <?php
 require_once('db_config.php');
 
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+ } 
+
+else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+else {
+    echo "Wrong API Key provided.";
+}
+$conn->close();
+
+
 $qry_user_id_user mysqli_query($con," SELECT `user.user_id` FROM user WHERE user.api_key = `{$post_api_key};`");
 $qry_sensor_id_user mysqli_query($con," SELECT `sensor.user_id` FROM sensor WHERE sensor.user_id = `{$post_sensor_id};`");
 
@@ -10,38 +26,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $sensorid = test_input($_POST[""]);
         $lichtsterkte = test_input($_POST["lux"]);
        
-        // Create connection
-        $conn = new mysqli($servername, $username, $password, $dbname);
-        // Check connection
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        } 
-        
         $sql = "INSERT INTO sensor (sensorid,lichtsterkte)
-        VALUES ('" . $sensorid . "',  '" . $lichtsterkte . "')";
-        
-        if ($conn->query($sql) === TRUE) {
-            echo "New record created successfully";
-        } 
-        else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
+        VALUES NOW ('" . $sensorid . "',  '" . $lichtsterkte . "')";
         }
-    
-        $conn->close();
-    }
-   
-    else {
-        echo "Wrong API Key provided.";
-    }
-
 }
-else {
-    echo "No data posted with HTTP POST.";
-}
-
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} 
 function test_input($data) {
     $data = trim($data);
     $data = stripslashes($data);
     $data = htmlspecialchars($data);
     return $data;
 }
+?>
